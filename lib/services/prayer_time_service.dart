a754f1b7eda0cb6b1c722518c107f7a4951d300d
Hadi Horani <hadihorani@hotmail.com>
2025-08-17 00:21:48 +0200

    dependency clean-up

--- lib/services/prayer_time_service.dart
+++ lib/services/prayer_time_service.dart
@@ -1,5 +1,6 @@
 import 'package:geolocator/geolocator.dart';
 import 'package:adhan/adhan.dart';
+import 'dart:io' show Platform;
 
 class PrayerTimeService {
   Future<PrayerTimes> getPrayerTimes() async {
@@ -12,14 +13,20 @@ class PrayerTimeService {
     Position? position;
     position = await Geolocator.getLastKnownPosition();
 
-    position ??= await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.medium,
-    );
+    if (position == null) {
+      final LocationSettings locationSettings = Platform.isAndroid
+          ? AndroidSettings(accuracy: LocationAccuracy.medium)
+          : AppleSettings(accuracy: LocationAccuracy.medium);
 
-    final userCoordinates = Coordinates(position.latitude, position.longitude);
+      position = await Geolocator.getCurrentPosition(
+        locationSettings: locationSettings,
+      );
+    }
+
+    final myCoordinates = Coordinates(position.latitude, position.longitude);
     final params = CalculationMethod.muslim_world_league.getParameters();
     params.madhab = Madhab.shafi;
-    final prayerTimes = PrayerTimes.today(userCoordinates, params);
+    final prayerTimes = PrayerTimes.today(myCoordinates, params);
 
     return prayerTimes;
   }
